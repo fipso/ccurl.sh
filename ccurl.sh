ffa789be9356e2a7023e35383931b992dd1de963
Paras Singh Laddi <8970106+parasdsingh@users.noreply.github.com>
2023-04-02 02:50:56 +0530

    Improvements suggested by GPT-3.5
    Changelog:
    - Check if jq and websocat commands are available before proceeding
    - Add double quotes around the $1 parameter in the curl command to handle cases where it contains spaces
    - Modify the command that gets the URL count to handle cases where there are multiple tabs/workers
    - Fix the command that creates the cookies string to handle cases where there are spaces or special characters in cookie values
    - Use set -euo pipefail to enable error checking
    - Use printf instead of echo for consistent behavior
    - Use readonly to define constant variables
    - Use functions to encapsulate functionality
    - Use $() instead of backticks for command substitution
    - Use [[ ]] instead of [ ] for more functionality
    - Use ${var:?} to fail if variable is undefined
    - Use heredoc for readability
    - Use a more specific pattern for tr to remove newlines from cookies
    - Use -r flag with read to prevent backslashes from being interpreted as escape characters
    - Use local keyword to declare all function variables
    - Add error checking for curl command

--- ccurl.sh
+++ ccurl.sh
@@ -1,21 +1,54 @@
 #!/bin/bash
 
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 [Tab URL Prefix] [cURL command ...]"
+set -euo pipefail
+
+# Define constant variables
+readonly JQ_CMD="jq"
+readonly WEBSOCAT_CMD="websocat"
+readonly TAB_URL_PREFIX="${1:?Usage: $0 [Tab URL Prefix] [cURL command ...]}"
+
+# Check if required commands are installed
+command -v "$JQ_CMD" &> /dev/null || {
+    printf "%s command not found. Please install it and try again.\n" "$JQ_CMD"
+    exit 1
+}
+
+command -v "$WEBSOCAT_CMD" &> /dev/null || {
+    printf "%s command not found. Please install it and try again.\n" "$WEBSOCAT_CMD"
+    exit 1
+}
+
+# Get the WebSocketDebuggerUrl for the tab with the specified prefix
+get_debug_url() {
+    local debug_url
+    debug_url=$(curl "http://127.0.0.1:9222/json" -s | "$JQ_CMD" -r --arg prefix "$TAB_URL_PREFIX" '.[] | select(.url | startswith($prefix)) | .webSocketDebuggerUrl')
+    if [[ -z $debug_url || $debug_url != ws* ]]; then
+        printf "Could not find tab starting with '%s'. Is chrome running ?\n" "$TAB_URL_PREFIX"
+        exit 1
+    fi
+    printf '%s\n' "$debug_url"
+}
+
+# Get the cookies for the tab with the specified WebSocketDebuggerUrl
+get_cookies() {
+    local cookies
+    cookies=$(cat <<-EOF | "$WEBSOCAT_CMD" -t - "$1" | "$JQ_CMD" -r '.result.cookies[] | "\(.name)=\(.value | @uri)";' | tr -d '\n\r'
+    { "id":2, "method":"Network.getCookies", "params":{} }
+EOF
+)
+    printf '%s\n' "$cookies"
+}
+
+# Get the debug url for the tab
+debug_url=$(get_debug_url)
+
+# Get the cookies for the tab
+cookies=$(get_cookies "$debug_url")
+
+# Execute the cURL command with the cookies and capture the output
+if ! output=$(curl -sSL -H "Cookie: $cookies" "${@:2}" 2>&1); then
+    printf "cURL command failed: %s\n" "$output"
     exit 1
 fi
 
-DEBUG_URL=$(curl "http://127.0.0.1:9222/json" -s | jq -r ".[] | select(.url | startswith(\"$1\")) | .webSocketDebuggerUrl")
-if ! [[ "$DEBUG_URL" =~ ^ws.* ]]; then
-    echo "Could not find tab starting with '$1'. Is chrome running ?"
-    exit 1
-fi
-
-URL_COUNT=$(echo "$DEBUG_URL" | tr -cd '\n' | wc -c)
-if [[ "$URL_COUNT" -gt 1 ]]; then
-    echo "Pattern '$1' is not precise enough. Multiple tabs/workers where found"
-    exit 1
-fi
-
-COOKIES=$(echo '{ "id":2, "method":"Network.getCookies", "params":{} }' | websocat -t - $DEBUG_URL | jq -r '.result.cookies[] | "\(.name)=\(.value)"' | tr '\n' ';' | sed 's/\;$/\n/')
-curl -H "Cookie: $COOKIES" "${@:2}"
+printf '%s\n' "$output"
